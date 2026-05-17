9bddb3222fde066604c913ccbb0ed6f414a6067a
DiachenkoDenis <tv42.diachenko.denys@gmail.com>
2026-05-18 01:11:11 +0300

    Update server.js

--- Lab6/server.js
+++ Lab6/server.js
@@ -1,1 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
 
+const authRoutes = require('./routes/auth');        
+const batteryRoutes = require('./routes/battery');  
+const { httpsRedirect } = require('./middleware/security'); 
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(httpsRedirect); 
+
+
+app.use('/auth', authRoutes);
+app.use('/api/battery', batteryRoutes);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`BESS server running on port ${PORT}`);
+});
